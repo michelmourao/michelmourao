873af9c680261bf13eb64fca448cd4a059c55bc1
michelmourao <michelmourao1@gmail.com>
2024-08-05 15:25:07 -0300

    Startig with vs

---
+++ C#/First API/FirstAPI.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MeuProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Mike!");
+        }
+    }
+}
