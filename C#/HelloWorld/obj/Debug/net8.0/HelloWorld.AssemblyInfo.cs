0fd34cf9002867b33d758208db2d8dc231d600b9
michelmourao <michelmourao1@gmail.com>
2024-08-14 12:53:43 -0300

    Studing FastAPI

--- C#/HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ C#/HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d8b8b7e2eccb79e5bed65836fcc8f3fb242d872")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6213f2f5d98fed4ec3f983d27d46623447484d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
